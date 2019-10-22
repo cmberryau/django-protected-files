d6c17aea43228df53b31b0404a45444e36a8cf4e
Chris Berry <chris@chrisberry.com.au>
2019-10-22 21:15:23 +1100

    removed abstract properties due to issues with cached_property

--- protected_files/storages.py
+++ protected_files/storages.py
@@ -3,41 +3,25 @@ from django.utils.functional import cached_property
 from django.core.files.storage import FileSystemStorage
 
 
-class ProtectedStorage(FileSystemStorage):
-    @property
-    def _root(self):
-        raise NotImplementedError
-
-    @property
-    def _url(self):
-        raise NotImplementedError
-
+class ProtectedMediaStorage(FileSystemStorage):
     @cached_property
     def base_location(self):
-        return self._value_or_setting(self._location, self._root)
+        return self._value_or_setting(self._location, settings.PROTECTED_MEDIA_ROOT)
 
     @cached_property
     def base_url(self):
         if self._base_url is not None and not self._base_url.endswith('/'):
             self._base_url += '/'
-        return self._value_or_setting(self._base_url, self._url)
+        return self._value_or_setting(self._base_url, settings.PROTECTED_MEDIA_URL)
 
 
-class ProtectedMediaStorage(ProtectedStorage):
-    @property
-    def _url(self):
-        return settings.PROTECTED_MEDIA_ROOT
+class ProtectedStaticStorage(FileSystemStorage):
+    @cached_property
+    def base_location(self):
+        return self._value_or_setting(self._location, settings.PROTECTED_STATIC_ROOT)
 
-    @property
-    def _root(self):
-        return settings.PROTECTED_MEDIA_URL
-
-
-class ProtectedStaticStorage(ProtectedStorage):
-    @property
-    def _url(self):
-        return settings.PROTECTED_STATIC_ROOT
-
-    @property
-    def _root(self):
-        return settings.PROTECTED_STATIC_URL
+    @cached_property
+    def base_url(self):
+        if self._base_url is not None and not self._base_url.endswith('/'):
+            self._base_url += '/'
+        return self._value_or_setting(self._base_url, settings.PROTECTED_STATIC_URL)
